e7968100ba0e7b2e03f5e6bd4f0c37bd3c50ee1e
Sanatan <sanatan.iit@gmail.com>
2017-08-09 06:18:49 +0530

    Fixed React Warning about dangerouslySetInnerHTML

--- src/components/Markdown_Preview/Markdown_Preview.js
+++ src/components/Markdown_Preview/Markdown_Preview.js
@@ -3,8 +3,7 @@ import marked from 'marked'
 import './Markdown_Preview.css'
 
 const Preview = ({markdown}) =>
-  <div
-    className='preview'>
+  <div className='preview'>
     <div dangerouslySetInnerHTML={{__html: marked(markdown)}}></div>
  </div>
 
